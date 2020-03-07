0923d5db46cd91e7cec348342af15674011e3a2f
GravyHTX <godisgravy@gmail.com>
2020-03-06 23:08:02 -0600

    Who fkn knows.....

--- server.js
+++ server.js
@@ -9,20 +9,10 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// $saveNoteBtn.on("click", function(event) {
-//   event.preventDefault();
-//   var newNote = {
-//     title: $noteTitle.val(),
-//     text: $noteText.val(),
-//     // id: $("#age").val(),
-//   };
+// var notes = []
 
-//   // Question: What does this code do??
-//   $.post("/api/notes", newNote)
-//     .then(function(data) {
-//       console.log("notes.html", data);
-//       alert("Adding note...");
-//     });
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./public/index.html"));
 // });
 
 // app.get("/", function(req, res) {
@@ -39,6 +29,10 @@ app.use(express.json());
 //   res.json(notes);
 // })
 
+// app.post("/api/notes", function(req,res){
+//   notesArr.push(req.body);
+//   fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(notes))
+// })
 
 // require("/public/assets/js/index")(app);
 // // require("./routes/htmlRoutes")(app);
@@ -48,56 +42,56 @@ app.use(express.json());
 //   });
   
 
-  // var notes = [];
+  var notes = [];
   
   // Routes
   // =============================================================
   
   // Basic route that sends the user first to the AJAX Page
-  // app.get("/", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "index.html"));
-  // });
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
   
-  // app.get("/add", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "notes.html"));
-  // });
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+  });
   
   // Displays all notes
-  // app.get("/api/notes", function(req, res) {
-  //   return res.json(notes);
-  // });
+  app.get("/api/notes", function(req, res) {
+    return res.json(notes);
+  });
   
-  // // Displays a single note, or returns false
-  // app.get("/api/notes/:id", function(req, res) {
-  //   var chosen = req.params.note;
+  // Displays a single note, or returns false
+  app.get("/api/notes/:id", function(req, res) {
+    var chosen = req.params.note;
   
-  //   console.log(chosen);
+    console.log(chosen);
   
-  //   for (var i = 0; i < notes.length; i++) {
-  //     if (chosen === notes[i].routeName) {
-  //       return res.json(notes[i]);
-  //     }
-  //   }
+    for (var i = 0; i < notes.length; i++) {
+      if (chosen === notes[i].routeName) {
+        return res.json(notes[i]);
+      }
+    }
   
-  //   return res.json(false);
-  // });
+    return res.json(false);
+  });
   
-  // // Create New notes - takes in JSON input
-  // app.post("/api/notes", function(req, res) {
-  //   // req.body hosts is equal to the JSON post sent from the user
-  //   // This works because of our body parsing middleware
-  //   var newnote = req.body;
+  // Create New notes - takes in JSON input
+  app.post("/api/notes", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newnote = req.body;
   
-  //   // Using a RegEx Pattern to remove spaces from newnote
-  //   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  //   newnote.routeName = newnote.name.replace(/\s+/g, "");
+    // Using a RegEx Pattern to remove spaces from newnote
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newnote.routeName = newnote.name.replace(/\s+/g, "");
   
-  //   console.log(newnote);
+    console.log(newnote);
   
-  //   notes.push(newnote);
+    notes.push(newnote);
   
-  //   res.json(newnote);
-  // });
+    res.json(newnote);
+  });
   
   // Starts the server to begin listening
   // =============================================================
